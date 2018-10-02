e4122e47db4d7c90415a923d6250e416f62ec4a1
Ken Moore <moorekou@gmail.com>
2018-10-02 09:30:27 -0400

    Update detect-best-driver.sh
    Make it take an optional input for checking particular devices

--- core-files/usr/local/share/trident/scripts/detect-best-driver.sh
+++ core-files/usr/local/share/trident/scripts/detect-best-driver.sh
@@ -2,12 +2,17 @@
 ##############
 # Quick script to probe the hardware and spit out the recommended driver for Xorg
 ##############
+# Input 1 (optional): Device to probe (vgapci0 by default)
+_device="$1"
+if [ -z "${_device}" ] ; then
+  _device="vgapci0"
+fi
 
 # First detect if the boottype is "UEFI" or not (legacy)
 efi=`sysctl -nq machdep.bootmethod`
 # Detect the vendor/device info for the graphics card
-vendor=`pciconf -lv vgapci0 | grep vendor | cut -d \' -f 2`
-device=`pciconf -lv vgapci0 | grep device | cut -d \' -f 2`
+vendor=`pciconf -lv "${_device}" | grep vendor | cut -d \' -f 2`
+device=`pciconf -lv "${_device}" | grep device | cut -d \' -f 2`
 #  -- NOTE: For each match listed here, add the vendor -> driver logic below as well
 MATCHES="nvidia:nvidia intel:intel innotek:vbox amd:amd vmware:vmware"
 for match in ${MATCHES}
