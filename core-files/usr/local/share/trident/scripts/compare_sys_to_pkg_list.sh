79d4c7a9ce960d931fc0321f2da4ec593df76af0
Ken Moore <moorekou@gmail.com>
2018-10-25 07:52:13 -0400

    Update the pkg comparison script: if "stage" or "release" is given as input, automatically fetch the file from the pkg server. If no input given, assume release. If invalid file given, throw an error and don't output anything

--- core-files/usr/local/share/trident/scripts/compare_sys_to_pkg_list.sh
+++ core-files/usr/local/share/trident/scripts/compare_sys_to_pkg_list.sh
@@ -7,8 +7,25 @@
 #  $1: path to package.list file
 #============
 listfile=$1
+if [ -z "${listfile}" ] ; then
+  listfile="release"
+fi
+
+
+if [ "${listfile}" = "release" ] || [ "${listfile}" = "stage" ] ; then
+  tmpfile="/tmp/.tmppkg_compare.list"
+  echo "Fetching latest pkg.list from Project Trident: ${listfile}"
+  fetch "http://pkg.project-trident.org/iso/${listfile}/pkg.list" -o "${tmpfile}"
+  listfile="${tmpfile}"
+fi
+
+if [ ! -e "${listfile}" ] ; then
+  echo "[ERROR] Could not find pkg.list file: ${listfile}"
+  return 1
+fi
 
 #Make sure we skip all the base packages
+echo "Scanning pkg.list for missing packages...."
 _missing=0
 for _pkg in `pkg query -a %n | grep -v -E "^(FreeBSD|TrueOS|OS)-"`
 do
@@ -24,4 +41,9 @@ if [ ${_missing} -gt 0 ] ; then
 ========${_missinglist}"
 else
   echo "No Missing Packages!"
+fi
+
+#Cleanup temporary download file as needed
+if [ -e "${tmpfile}" ] && [ -n "${tmpfile}" ] ; then
+  rm "${tmpfile}"
 fi
