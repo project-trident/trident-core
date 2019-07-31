cc12bf8487103f09f7d44decc21b31fd611079c6
Ken Moore <ken@ixsystems.com>
2019-07-31 14:32:24 -0400

    Add a new "scan-wifi-drivers.sh" script. This will iterate through all the network modules and see if any of them enable the discovery of a new wifi device, and then set that module to be automatically loaded on every bootup.

---
+++ core-files/usr/local/share/trident/scripts/scan-wifi-drivers.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+# Quick script to scan for available wifi devices and then load/unload the different modules to see which ones are needed.
+
+curdev=$(sysctl -n net.wlan.devices)
+
+for module in `ls /boot/kernel/if_*.ko`
+do
+  modname=$( basename ${module} | cut -d . -f 1 )
+  kldstat -q -m "${modname}"
+  if [ $? -eq 1 ] ; then
+    #Module not currently loaded, test it
+    kldload -nq "${modname}"
+    if [ $? -eq 0 ] ; then
+      if [ "${curdev}" != "$()" ] ; then
+        # New devices found - add it to loader.conf
+        echo "Found new wifi devices with module: ${modname}"
+        echo '${modname}_load="YES"' >> /boot/loader.conf
+      fi
+      kldunload -q "${modname}"
+    fi
+  fi
+done
