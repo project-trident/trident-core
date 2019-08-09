8cdbca55d9b7669aaf575435f24b2db0a4cf1590
Ken Moore <ken@ixsystems.com>
2019-08-09 08:12:07 -0400

    Ensure the XDG_RUNTIME_DIR actually exists and is read/write only by the user.

--- core-files/usr/local/share/trident/xstartup/01_xdg-user-dirs.sh
+++ core-files/usr/local/share/trident/xstartup/01_xdg-user-dirs.sh
@@ -6,6 +6,12 @@ export XDG_CONFIG_DIRS="/usr/local/etc/xdg"
 export XDG_CONFIG_HOME="${HOME}/.config"
 export XDG_RUNTIME_DIR="/tmp/.runtime-${USERNAME}"
 
+#Ensure the XDG runtime directory exists and is only read/write by the user
+if [ ! -d "${XDG_RUNTIME_DIR}" ] ; then
+  mkdir -m 700 "${XDG_RUNTIME_DIR}"
+  chown "${USERNAME}:${USERNAME}" "${XDG_RUNTIME_DIR}"
+fi
+
 #Check the standardized user directories and create them as needed
 if [ -x /usr/local/bin/xdg-user-dirs-update ] ; then
   xdg-user-dirs-update
