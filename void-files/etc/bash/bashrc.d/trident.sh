8447d927c858bc14d830b64119fe3d96ac24d616
Ken Moore <moorekou@gmail.com>
2020-03-25 12:13:27 -0400

    Add bash setup file. Move all the FreeBSD-specific dirs into a new "obsolete" dir

---
+++ void-files/etc/bash/bashrc.d/trident.sh
@@ -0,0 +1,7 @@
+export PS1="\[\e[34m\]\u\[\e[m\]|\[\e[36m\]\w\[\e[m\]> "
+alias about="neofetch"
+alias ls="ls --color"
+export HISTSIZE=50
+export HISTCONTROL=ignoreboth
+bind '"\e[A": history-search-backward'
+bind '"\e[B": history-search-forward'
