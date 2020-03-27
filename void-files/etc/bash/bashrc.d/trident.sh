3d7d5088c5a3dff6728ffb340aeb49780bbf7775
Ken Moore <moorekou@gmail.com>
2020-03-27 08:33:15 -0400

    Update the default bash prompt. Color the "root" user with red instead of blue

--- void-files/etc/bash/bashrc.d/trident.sh
+++ void-files/etc/bash/bashrc.d/trident.sh
@@ -1,4 +1,8 @@
-export PS1="\[\e[34m\]\u\[\e[m\]|\[\e[36m\]\w\[\e[m\]> "
+if [ "${USER}" = "root" ] ; then
+  export PS1="\[\e[31m\]\u\[\e[m\]|\[\e[36m\]\w\[\e[m\]> "
+else
+  export PS1="\[\e[34m\]\u\[\e[m\]|\[\e[36m\]\w\[\e[m\]> "
+fi
 alias about="neofetch"
 alias ls="ls --color"
 export HISTSIZE=50
