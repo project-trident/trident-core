788d11430fee70bf01f337cba7c1c429ab11f598
Ken Moore <moorekou@gmail.com>
2020-05-01 08:10:45 -0400

    Add GOPATH to the search paths for bins. Update xbps package templates

--- void-files/etc/bash/bashrc.d/trident.sh
+++ void-files/etc/bash/bashrc.d/trident.sh
@@ -5,6 +5,7 @@ else
   export PS1="\[\e[34m\]\u\[\e[m\]|\[\e[36m\]\w\[\e[m\]> "
   export GOPATH=${HOME}/.local/go
 fi
+export PATH="${PATH}:${GOPATH}/bin"
 alias about="neofetch"
 alias ls="ls --color"
 export HISTSIZE=50
