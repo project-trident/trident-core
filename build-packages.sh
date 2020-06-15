a7065d538eb7f7a470bdd3a09cc4c2d174e4feed
Ken Moore <moorekou@gmail.com>
2020-06-15 14:59:02 -0400

    Fix up the build script and trident-base template

--- build-packages.sh
+++ build-packages.sh
@@ -4,11 +4,13 @@
 buildrepodir="${1}"
 templatedir="$(dirname ${0})/xbps"
 
+pkgs="trident-base trident-core trident-desktop"
 #Copy over the package templates
-cp -R ${templatedir}/trident-core ${buildrepodir}/srcpkgs/trident-core
+for pkg in ${pkgs}
+do
+cp -Rf ${templatedir}/${pkg} ${buildrepodir}/srcpkgs/${pkg}
 if [ $? -ne 0 ] ; then exit 1 ; fi
-cp -R ${templatedir}/trident-desktop ${buildrepodir}/srcpkgs/trident-desktop
-if [ $? -ne 0 ] ; then exit 1 ; fi
+done
 
 # Update the checksums in the packages (always return errors when changing the file)
 cd ${buildrepodir} && xgensum -i srcpkgs/trident-core
@@ -21,12 +23,17 @@ cd ${buildrepodir} && xgensum -c srcpkgs/trident-desktop
 if [ $? -ne 0 ] ; then exit 1 ; fi
 
 #Build the packages
-cd ${buildrepodir} && ./xbps-src pkg trident-desktop
-if [ $? -ne 0 ] ; then exit 1 ; fi
+for pkg in ${pkgs}
+do
+  cd ${buildrepodir} && ./xbps-src pkg ${pkg}
+  if [ $? -ne 0 ] ; then exit 1 ; fi
+done
 
 #Copy the new packages to this directory
-cp ${buildrepodir}/hostdir/binpkgs/trident-core-*.noarch.xbps .
-cp ${buildrepodir}/hostdir/binpkgs/trident-desktop-*.noarch.xbps .
+for pkg in ${pkgs}
+do
+  cp ${buildrepodir}/hostdir/binpkgs/${pkg}-*.noarch.xbps .
+done
 
 # Now cleanup
 cd ${buildrepodir} && ./xbps-src clean
