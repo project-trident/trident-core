11716a00cd80cca64becdb6545ba03d097ed5ada
Ken Moore <moorekou@gmail.com>
2020-06-15 15:30:43 -0400

    A bit more tweaks to the build script

--- build-packages.sh
+++ build-packages.sh
@@ -4,12 +4,19 @@
 buildrepodir="${1}"
 templatedir="$(dirname ${0})/xbps"
 
+if [ -z "${buildrepodir}" ] ; then
+  echo "Usage:  build-packages.sh [build-repo dir]"
+  exit 1
+fi
 pkgs="trident-base trident-core trident-desktop"
 #Copy over the package templates
 for pkg in ${pkgs}
 do
-cp -Rf ${templatedir}/${pkg} ${buildrepodir}/srcpkgs/${pkg}
-if [ $? -ne 0 ] ; then exit 1 ; fi
+  if [ -d "${buildrepodir}/srcpkgs/${pkg}" ] ; then
+    rm -r "${buildrepodir}/srcpkgs/${pkg}"
+  fi
+  cp -Rf ${templatedir}/${pkg} ${buildrepodir}/srcpkgs/${pkg}
+  if [ $? -ne 0 ] ; then exit 1 ; fi
 done
 
 # Update the checksums in the packages (always return errors when changing the file)
@@ -32,7 +39,11 @@ done
 #Copy the new packages to this directory
 for pkg in ${pkgs}
 do
+  echo "Copying package: ${pkg}"
   cp ${buildrepodir}/hostdir/binpkgs/${pkg}-*.noarch.xbps .
+  if [ $? -ne 0 ] ; then
+    echo "[ERROR] Could not copy package: ${pkg}"
+  fi
 done
 
 # Now cleanup
